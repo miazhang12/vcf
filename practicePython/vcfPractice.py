2db80c107b2f3271587abb61730a97ff55c952f7
miazhang12 <miazzhang@gmail.com>
2017-03-06 23:20:49 -0500

    Added a criteria to print certain records

--- practicePython/vcfPractice.py
+++ practicePython/vcfPractice.py
@@ -1,14 +1,17 @@
 import vcf
 import sys
+import string
 
 def print_vcf():
-	filename = raw_input("Enter Filename: ")
-	vcf_reader = vcf.Reader(open(filename))
-	record = next(vcf_reader)
-	print record.ALT
-	
+	filename = raw_input("Enter Filename: ") #user input for the filename
+	vcf_reader = vcf.Reader(open(filename)) #open file
+	for record in vcf_reader: #loops through all the records in vcf
+		for i in record.ALT: 
+			if i == 'T' and record.REF == 'C': #filter out the records with T ALT and C REF
+				print record #print out the record if meet the criteria
+				
 def main():
-	print_vcf()
+	print_vcf()	#call print_vcf function
 
 if __name__ == "__main__":
 	main()
